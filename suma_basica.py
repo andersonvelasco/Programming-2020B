1b3487b96664039eca3f743b3d909947d41c61cd
andersonvelasco <anderpik94@gmail.com>
2020-09-23 19:23:19 -0500

    carga calculadora basica

---
+++ suma_basica.py
@@ -0,0 +1,11 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n1=5
+>>> n2=3
+>>> suma=0
+>>> suma=n1+n2
+>>> print ("la suma de ",n1, "+", n2,"es igual a: " suma)
+SyntaxError: invalid syntax
+>>> print ("la suma de ",n1,"+",n2,"es igual a: ",suma)
+la suma de  5 + 3 es igual a:  8
+>>> 
