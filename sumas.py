b8bd1c81c0ea0ec2400f17e9709e94c0e8fa0d1b
andersonvelasco <anderpik94@gmail.com>
2020-09-25 20:58:28 -0500

    suma_tres_numeros

---
+++ sumas.py
@@ -0,0 +1,16 @@
+#Script:sumas
+'''
+suma 3 numeros
+ingresados por el usuario
+'''
+#INPUTS
+print("Ingrese n1: ")
+n1=int(input())
+print("Ingrese n1: ")
+n2=int(input())
+print("Ingrese n1: ")
+n3=int(input())
+#PROCESS
+suma=n1+n2+n3
+#OUTPUTS
+print ("la suma de ",n1,"+",n2,"+",n3,"es igual a: ",suma)
