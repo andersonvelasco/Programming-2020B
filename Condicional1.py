8b6ef7af7a9d5a96f991d33c8e9c5a0ef7de1e7b
andersonvelasco <anderpik94@gmail.com>
2020-09-30 20:11:01 -0500

    Condicion1

---
+++ Condicional1.py
@@ -0,0 +1,13 @@
+'''Script que identifica si un
+número ingresado por el usuario
+es par'''
+
+num=0
+
+print ("Ingrese un número entero: ")
+num=int(input())
+
+if num % 2 == 0 :
+    print("El número es PAR")
+else:
+    print ("El número es impar")
