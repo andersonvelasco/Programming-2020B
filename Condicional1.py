ae9bacedddb716a269ced76313dc4afd26676a71
andersonvelasco <anderpik94@gmail.com>
2020-10-01 19:15:43 -0500

    contitional_1_=>no_par_o_impar

--- Condicional1.py
+++ Condicional1.py
@@ -2,12 +2,17 @@
 número ingresado por el usuario
 es par'''
 
+import os
+os.system("cls") # thsi kine let you clear screen.
+
 num=0
 
 print ("Ingrese un número entero: ")
 num=int(input())
-
-if num % 2 == 0 :
-    print("El número es PAR")
-else:
-    print ("El número es impar")
+if num >= 0 :
+    if num % 2 == 0 :
+        print("El número es PAR")
+    else:
+        print ("El número es impar")
+else :
+    print(":::NO SE ACEPTAN VALORES NEGATIVOS")
