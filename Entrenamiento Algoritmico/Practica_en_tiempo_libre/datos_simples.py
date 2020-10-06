6ef834dc3eb4c203ddcbb0581ebe1e9f0a2222de
andersonvelasco <anderpik94@gmail.com>
2020-10-06 12:41:07 -0500

    ejercicios_practica_work

---
+++ Entrenamiento Algoritmico/Practica_en_tiempo_libre/datos_simples.py
@@ -0,0 +1,11 @@
+'''programa que pregunte el nombre del usuario en la consola 
+y un número entero e imprima por pantalla en líneas distintas
+el nombre del usuario tantas veces como el número introducido.'''
+
+import os
+os.system("cls") # thsi kine let you clear screen.
+
+
+name = input("¿Cómo te llamas? ")
+n = input("Introduce un número entero: ")
+print((name + "\n") * int(n))
