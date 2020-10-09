86642df87b2151025a920cbdbfd9a3c135e83f74
andersonvelasco <anderpik94@gmail.com>
2020-10-08 19:46:35 -0500

    ciclo operaciones

--- Entrenamiento Algoritmico/operacionesbasicas.py
+++ Entrenamiento Algoritmico/operacionesbasicas.py
@@ -3,12 +3,51 @@
 import os
 os.system("cls") # this line let you clear screen.
 
-print ("Programa para operaciones aritmeticas básicas")
+'''print ("Programa para operaciones aritmeticas básicas")
 print ("[1] Sumar")
 print ("[2] Restar")
 print ("[3] Multiplicar")
-print ("[4] Dividir")
-op=int(input("Seleccione la operación : "))
+print ("[4] Dividir")'''
+print ("[1] Operar")
+print ("Cualquier número para Salir")
+i=int(input("Digite operador: "))
+while i==1:
+    print("Operadores")
+    print ("[1] Sumar")
+    print ("[2] Restar")
+    print ("[3] Multiplicar")
+    print ("[4] Dividir")
+    print ("[5] Salir")
+    op=int(input("Digite Operación: "))     
+    if op==1:
+        print ("[1] SUMA")
+        num1=int(input("Ingrese primer número: "))
+        num2=int(input("Ingrese segundo número: "))
+        print(num1," + ", num2," = ",num1+num2) 
+    elif op==2:
+        print ("[2] RESTA")
+        num1=int(input("Ingrese primer número: "))
+        num2=int(input("Ingrese segundo número: "))
+        print(num1," - ", num2," = ",num1-num2)
+    elif op==3:
+        print ("[3] Multiplicación")
+        num1=int(input("Ingrese primer número: "))
+        num2=int(input("Ingrese segundo número: "))
+        print(num1," X ", num2," = ",num1*num2)
+    elif op==4:
+        print ("[4] División")
+        num1=int(input("Ingrese primer número: "))
+        num2=int(input("Ingrese segundo número: "))
+        print(num1," / ", num2," = ",num1/num2)
+    elif op==5:
+        i=i+1  
+    else :
+        print("Operación no encontrada")
+    i=i
+print("Fin ejecución")
+
+
+'''op=int(input("Digite operación"))
 if op==1:
     print ("[1] SUMA")
     num1=int(input("Ingrese primer número: "))
@@ -32,4 +71,4 @@ elif op==4:
        
 else :
     print("Operación no encontrada")
-    
+'''
