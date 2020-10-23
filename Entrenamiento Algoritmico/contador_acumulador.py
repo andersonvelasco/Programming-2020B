ff01e9e775e0927deb0803f91e03dc360faa94a7
andersonvelasco <anderpik94@gmail.com>
2020-10-22 19:14:50 -0500

    contador_acumulador_mensajes_contadores_igualados_cero

--- Entrenamiento Algoritmico/contador_acumulador.py
+++ Entrenamiento Algoritmico/contador_acumulador.py
@@ -11,14 +11,16 @@ Procesos:
 5. Suma total de los pares
 6. Suma total de los impares
 7. Suma total de todos los números
+8. Cual es promedio
 
 '''
 import os
 os.system("cls")
 num=int(input("Ingrese No. : "))
+print("\n:::::LISTA GENERADA:::::")
 i=1
-contp=1
-conti=1
+contp=0
+conti=0
 acump=0 #Acumulador inicializado en 0 para guardar y sumar los no. pares
 acumi=0 #Acumulador inicializado en 0 para guardar y sumar los no. impares
 acumt=0 #Acumulador inicializado en 0 para guardas la suma de todos los numeros OPCIONAL
@@ -32,13 +34,15 @@ while i<=num :# si i es menos o igual a num (No. digitado por el usuario, entonc
         print("El No. listado es : ",i," y es impar")
         conti=conti+1 #contador para los numeros impares, incrementa en 1 cada vez que pasa por el ciclo
         acumi=acumi+i #inicia el acumulador total a registrar los valores de i impares
-    i=i+1 #contador encargado de romper el ciclo cuando 
+    i=i+1 #contador encargado de romper el ciclo cuando
+print("\n::::::REPORTE::::::")
 print("La cantidad de números generados op 1 es: ", num)
-print("La cantidad de números generados op 2 es: ", i - 1)
-print("La cantidad de números pares es : ", contp - 1)
-print("La cantidad de números impares es : ", conti - 1)
+#print("La cantidad de números generados op 2 es: ", i - 1)
+print("La cantidad de números pares es : ", contp)
+print("La cantidad de números impares es : ", conti)
 print("La suma de los no. pares es: ",acump)
-print("La suma de los no. pares es: ",acumi)
-print("La suma de todos los números op 1es : ",acump+acumi)#== acumt
-print("La suma de todos los números op 2 es : ",acumt)# == acumi+acump -- OPCIONAL
+print("La suma de los no. impares es: ",acumi)
+print("La suma de todos los números op 1 es : ",acump+acumi)#== acumt
+#print("La suma de todos los números op 2 es : ",acumt)# == acumi+acump -- OPCIONAL
+print("El promedio de los numeros generados es : ",acumt/i)
 
